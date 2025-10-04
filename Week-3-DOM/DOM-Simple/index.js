58e805cbddfd93ef4f33e217555244e92a670d6c
Pranav Awasarmol <pranav.awasarmol766@gmail.com>
2025-10-04 14:06:54 +0530

    Todos full app

--- Week-3-DOM/DOM-Simple/index.js
+++ Week-3-DOM/DOM-Simple/index.js
@@ -1,7 +1,30 @@
-let cnt=0;
-function deleteTodo(ind){
-    const el = document.getElementById("todo-"+ind);
-    el.parentNode.removeChild(el);
+let cnt = 1;
+
+function delTodo(ind) {
+  const el = document.getElementById(ind);
+  el.parentNode.removeChild(el);
 }
 
-setInterval(callback, 1000)
+function addTodo() {
+  const inpel = document.getElementById("inp");
+  const val = inpel.value.trim();
+  if (val === "") {
+    alert("Enter a valid ToDo");
+    return;
+  }
+  console.log(val);
+  const newDiv = document.createElement("div");
+  const d =
+    "<div>" +
+    val +
+    '<button onclick="delTodo(' +
+    cnt +
+    ')">Delete</button></div>';
+  newDiv.setAttribute("id", cnt);
+  cnt++;
+  newDiv.innerHTML = d;
+
+  document.querySelector("#todo").appendChild(newDiv);
+
+  inpel.value = "";
+}
