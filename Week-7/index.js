76b1c0522ac0efef68d1f9c478e6ffea3adf3fe4
elpranav05 <pranav.awasarmol766@gmail.com>
2026-02-15 19:26:57 +0530

    Zod-Validation

--- Week-7/index.js
+++ Week-7/index.js
@@ -1,189 +1,232 @@
-const express = require('express');
-const cors = require('cors');
-const {Usermodel, Todomodel} = require("./db")
-const jwt = require('jsonwebtoken');
-const { default: mongoose } = require('mongoose');
+const express = require("express");
+const bcrypt = require("bcrypt");
+const cors = require("cors");
+const { Usermodel, Todomodel } = require("./db");
+const jwt = require("jsonwebtoken");
+const { z } = require("zod");
+const { default: mongoose } = require("mongoose");
 require("dotenv").config();
 
-const {JWT_SECRET, MONGO_URL, auth} = require("./auth") 
+const { JWT_SECRET, MONGO_URL, auth } = require("./auth");
 
 const app = express();
-app.use(cors())
-mongoose.connect(MONGO_URL)
-.then(()=>{
+app.use(cors());
+mongoose
+  .connect(MONGO_URL)
+  .then(() => {
     console.log("DB Connected");
 
-    app.listen(3000,()=>{
-        console.log("Server running on 3000");
-    })
-})
-.catch(err=>{
-    console.log("Error:"+err);
-})
+    app.listen(3000, () => {
+      console.log("Server running on 3000");
+    });
+  })
+  .catch((err) => {
+    console.log("Error:" + err);
+  });
 
 app.use(express.json());
 
-app.post("/signup", async (req,res)=>{
-    
-    try {
-        const name = req.body.name;
-        const email = req.body.email;
-        const password = req.body.password;
-
-        await Usermodel.create({
-          name: name,
-          password: password,
-          email: email,
-        });
-        res.json({
-          msg: "You are signed up",
-        });
-        
-    } catch (error) {
-        res.status(500).json({
-          msg: "Something went wrong",
-        });
-    }
-
-    
-})
-
-app.post("/signin", async (req, res) => {
-
-    try {
-        const email = req.body.email;
-        const password = req.body.password;
-
-        const user = await Usermodel.findOne({
-          email: email,
-          password: password,
-        });
-
-        if (user) {
-          const token = jwt.sign(
-            {
-              id: user._id.toString(),
-            },
-            JWT_SECRET,
-          );
-
-          res.json({
-            token: token,
-          });
-        } else {
-          res.status(500).json({
-            msg: "Something went wrong",
-          });
-        }
-
-        
-    } catch (error) {
-        res.status(500).json({
-          msg: "Something went wrong",
-        });
-    }
-
-    
-
-});
-
-app.post("/todo",auth, async (req, res) => {
-
-    try {
-        const title = req.body.title;
-        const userId = req.userId;
-        const done = req.body.done;
-
-        await Todomodel.create({
-          title: title,
-          userId: userId,
-          done: done,
-        });
-        res.json({
-          msg: "Done!",
-        });
-        
-    } catch (error) {
-        res.status(500).json({
-          msg: "Something went wrong",
-        });
-    }
-});
-
-app.get("/todos", auth, async (req,res)=>{
-
+app.post("/signup", async (req, res) => {
   try {
-    const todos = await Todomodel.find({
-    userId:req.userId
-  })
+    const required = z.object({
+      email: z.string().min(5).max(100).email(),
+      password: z
+        .string()
+        .min(3)
+        .max(100)
+        .regex(/[a-z]/, "Must contain a lowercase letter")
+        .regex(/[A-Z]/, "Must contain an uppercase letter")
+        .regex(/[0-9]/, "Must contain a number")
+        .regex(/[^A-Za-z0-9]/, "Must contain a special character"),
+      name: z.string().min(3).max(30),
+    });
 
-  res.json({
-    todos:todos
-  })
-    
+    const parsing = required.safeParse(req.body);
+
+    if (!parsing.success) {
+      return res.json({
+        msg: "Invalid Credentials",
+        error:  parsing.error.issues[0].message
+,
+      });
+    }
+
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const hashedPass = await bcrypt.hash(password, 5);
+
+    await Usermodel.create({
+      name: name,
+      password: hashedPass,
+      email: email,
+    });
+
+    res.json({
+      msg: "You are signed up",
+    });
   } catch (error) {
     res.status(500).json({
-          msg: "Something went wrong",
-        });
+      msg: "Email already signed up",
+    });
   }
-})
+});
 
-app.delete("/delete/:id",auth, async (req,res)=>{
+app.post("/signin", async (req, res) => {
   try {
-    const deleted = await Todomodel.findOneAndDelete({
-      _id:req.params.id,
-      userId:req.userId
-    })
+    const required = z.object({
+      email: z.string().min(5).max(50).email(),
+      password: z
+        .string()
+        .min(3)
+        .max(30)
+        .regex(/[a-z]/, "Must contain lowercase letters")
+        .regex(/[A-Z]/, "Must contain uppercase letters")
+        .regex(/[0-9]/, "Must contain numbers")
+        .regex(/[^A-Za-z0-9]/, "Must contain a special character"),
+    });
 
-    if(!deleted){
-      return res.status(404).json({
-        msg:"Not allowed"
+    const parsing = required.safeParse(req.body);
+
+    if(!parsing.success){
+      return res.json({
+        msg:"Invalid Credentials",
+        error:parsing.error.issues[0].message
       })
     }
 
-    res.json({
-      msg:deleted,
-      asd:"asdasd"
-    })
-    
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const user = await Usermodel.findOne({
+      email: email,
+    });
+
+    const passMatch = await bcrypt.compare(password, user.password);
+
+    if (passMatch) {
+      const token = jwt.sign(
+        {
+          id: user._id.toString(),
+        },
+        JWT_SECRET,
+      );
+
+      res.json({
+        token: token,
+      });
+    } else {
+      return res.status(500).json({
+        msg: "Incorrect credentials",
+      });
+    }
   } catch (error) {
     res.status(500).json({
       msg: "Something went wrong",
     });
   }
-})
+});
 
-app.delete("/deleteall",auth, async(req,res)=>{
-
+app.post("/todo", auth, async (req, res) => {
   try {
-    
+    const check = z.object({
+      title: z.string().min(2)
+    })
+
+    const parsing = check.safeParse(req.body);
+
+    if(!parsing.success){
+      return res.json({
+        msg: "Invalid Credentials",
+        error: parsing.error.issues[0].message
+      });
+    }
+
+    const title = req.body.title;
+    const userId = req.userId;
+    const done = req.body.done;
+
+    await Todomodel.create({
+      title: title,
+      userId: userId,
+      done: done,
+    });
+    res.json({
+      msg: "Done!",
+    });
+  } catch (error) {
+    res.status(500).json({
+      msg: "Something went wrong",
+    });
+  }
+});
+
+app.get("/todos", auth, async (req, res) => {
+  try {
+    const todos = await Todomodel.find({
+      userId: req.userId,
+    });
+
+    res.json({
+      todos: todos,
+    });
+  } catch (error) {
+    res.status(500).json({
+      msg: "Something went wrong",
+    });
+  }
+});
+
+app.delete("/delete/:id", auth, async (req, res) => {
+  try {
+    const deleted = await Todomodel.findOneAndDelete({
+      _id: req.params.id,
+      userId: req.userId,
+    });
+
+    if (!deleted) {
+      return res.status(404).json({
+        msg: "Not allowed",
+      });
+    }
+
+    res.json({
+      msg: deleted,
+      asd: "asdasd",
+    });
+  } catch (error) {
+    res.status(500).json({
+      msg: "Something went wrong",
+    });
+  }
+});
+
+app.delete("/deleteall", auth, async (req, res) => {
+  try {
     const deleted = await Todomodel.deleteMany({
       userId: req.userId,
     });
 
-    if(deleted){
+    if (deleted) {
       res.json({
-        msg:"Deleted all",
-        deletedCount:deleted.deletedCount
-      })
+        msg: "Deleted all",
+        deletedCount: deleted.deletedCount,
+      });
     }
-    
   } catch (error) {
     res.status(500).json({
       msg: "Server error",
     });
   }
-  
-
-})
+});
 
 app.put("/edit/:id", auth, async (req, res) => {
-  const id = req.params.id;  
+  const id = req.params.id;
   const task = req.body.task;
   const resp = await Todomodel.findOneAndUpdate(
     { _id: id, userId: req.userId.toString() },
     { title: task },
-    
   );
 
   if (!resp) {
