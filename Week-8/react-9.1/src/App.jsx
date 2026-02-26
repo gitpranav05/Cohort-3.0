49c245170a25389788c84228811d03a80b8fd8d3
elpranav05 <pranav.awasarmol766@gmail.com>
2026-02-26 15:08:08 +0530

    Children

--- Week-8/react-9.1/src/App.jsx
+++ Week-8/react-9.1/src/App.jsx
@@ -1,38 +1,93 @@
-import { useEffect, useState } from "react";
-import { PostComponent } from "./components/PostComponent";
+import React from "react";
 
-function App() {
-  const [count, setCount] = useState(1);
-
-  function counter() {
-    setCount(function(curr){
-      return curr+1;
-    })
-  }
-
-  useEffect(function(){
-    setInterval(counter,1000);
-  }, [])
-
-
-
+const Card = ({ children }) => {
   return (
-    <div>
-      <div style={{backgroundColor: "red", borderRadius:20, width:20, paddingLeft: 10, marginLeft:30, paddingTop:10}}>
-        {count}
-      </div>
-      <img
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBFnSIAXp2wpKT9HzzaIYDg2Zomevux3lqNw&s"
-        style={{height:50}}
-      />
-  {/* <button onClick={counter}>Add</button> */}
-
+    <div
+      style={{
+        border: "1px solid #ccc",
+        borderRadius: "5px",
+        padding: "20px",
+        margin: "10px",
+        boxShadow: "2px 2px 5px rgba(0, 0, 0, 0.1)",
+      }}
+    >
+      {children}
     </div>
   );
+};
 
-}
+const App = () => {
+  return (
+    <div>
+      <Card>
+        <h2>Card Title</h2>
+        <p>This is some content inside the card.</p>
+      </Card>
+      <Card>
+        <h2>Another Card</h2>
+        <p>This card has different content!</p>
+      </Card>
+    </div>
+  );
+};
 
 export default App;
+
+// const [currTab, setTab] = useState(1);
+// const [loading, setLoading] = useState(true);
+// const [tabData, setTabData] = useState({});
+
+// useEffect(() => {
+//   setLoading(true);
+
+//   fetch("https://jsonplaceholder.typicode.com/todos/" + currTab).then(
+//     async (res) => {
+//       const json = await res.json();
+//       setTabData(json);
+//       setLoading(false);
+//     },
+//   );
+// }, [currTab]);
+
+// return (
+//   <div>
+//     <button
+//       onClick={() => {
+//         setTab(1);
+//       }}
+//       style={{ color: currTab == 1 ? "red" : "black" }}
+//     >
+//       Todo 1
+//     </button>
+//     <button
+//       onClick={() => {
+//         setTab(2);
+//       }}
+//       style={{ color: currTab == 2 ? "red" : "black" }}
+//     >
+//       Todo 2
+//     </button>
+//     <button
+//       onClick={() => {
+//         setTab(3);
+//       }}
+//       style={{ color: currTab == 3 ? "red" : "black" }}
+//     >
+//       Todo 3
+//     </button>
+//     <button
+//       onClick={() => {
+//         setTab(4);
+//       }}
+//       style={{ color: currTab == 4 ? "red" : "black" }}
+//     >
+//       Todo 4
+//     </button>
+
+//     <br />
+//     {loading ? "Loading..." : tabData.title}
+//   </div>
+// );
 
 // const [posts, setPosts] = useState([]);
 
@@ -55,7 +110,6 @@ export default App;
 //     desc: "Hello Pranav Awasarmol"
 //   }]);
 // }
-
 
 // return (
 //   <div style={{ background: "#dfe6e9", height: "100vh" }}>
