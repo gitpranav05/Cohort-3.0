41c51f859bc58889da56e82b202506256f8f4843
elpranav05 <pranav.awasarmol766@gmail.com>
2026-03-19 16:38:09 +0530

    Redux-Toolkit

---
+++ Week-11-redux-toolkit/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import AddTodos from './components/AddTodos'
+import Todos from './components/Todos'
+
+function App() {
+
+  return (
+    <div>
+      <h1>Learn about redux</h1>
+      <AddTodos/>
+      <Todos/>
+    </div>
+  )
+}
+
+export default App
