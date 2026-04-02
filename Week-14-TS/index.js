cb11dad0dec4f6ed3219c33cb01d61f6d666a014
elpranav05 <pranav.awasarmol766@gmail.com>
2026-04-02 18:01:05 +0530

    Typescript init

---
+++ Week-14-TS/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(name) {
+    console.log(`Hello ${name}`);
+}
+greet("Pranav");
+//# sourceMappingURL=index.js.map
