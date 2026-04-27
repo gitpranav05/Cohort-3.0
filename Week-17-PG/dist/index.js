201185bee62fff7af863d86bd571012ebec5c9a2
elpranav05 <pranav.awasarmol766@gmail.com>
2026-04-27 22:09:44 +0530

    asdasd

--- Week-17-PG/dist/index.js
+++ Week-17-PG/dist/index.js
@@ -1,5 +1,5 @@
 import { Client } from "pg";
-const pgClient = new Client("postgresql://neondb_owner:npg_F6PeoRAy4gIS@ep-misty-tree-amrqhn4g-pooler.c-5.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require");
+const pgClient = new Client();
 async function main() {
     await pgClient.connect();
     const resp = await pgClient.query("UPDATE users SET email='aastha@gmail.com' where id=3;");
--- Week-17-PG/src/index.ts
+++ Week-17-PG/src/index.ts
@@ -1,7 +1,6 @@
 import { Client } from "pg";
 
 const pgClient = new Client(
-  "postgresql://neondb_owner:npg_F6PeoRAy4gIS@ep-misty-tree-amrqhn4g-pooler.c-5.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
 );
 
 async function main() {
