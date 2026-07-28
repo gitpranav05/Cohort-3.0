98d960dca99f8f5b551ce846907b6ffec1a15d47
elpranav05 <pranav.awasarmol766@gmail.com>
2026-07-28 16:33:07 +0530

    Dynamic

--- week-20/app/blog/[postId]/page.tsx
+++ week-20/app/blog/[postId]/page.tsx
@@ -1,6 +1,21 @@
+import axios from "axios";
+
 async function page({ params }: { params: { postId: string } }) {
   const id = (await params).postId;
-  return <div>Blog Page {id} </div>;
+
+  const resp = await axios.get(
+    `https://jsonplaceholder.typicode.com/posts/${id}`,
+  );
+  const data = resp.data;
+  return (
+    <div>
+      Blog Page {id}
+      <br />
+      title - {data.title}
+      <br />  
+      body - {data.body}
+    </div>
+  );
 }
 
 export default page;
