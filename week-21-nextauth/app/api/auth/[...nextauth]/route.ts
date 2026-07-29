291e1d2ff71f94144497897165f85f354456c928
elpranav05 <pranav.awasarmol766@gmail.com>
2026-07-29 14:37:37 +0530

    next auth last

---
+++ week-21-nextauth/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,55 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+
+
+const handler = NextAuth({
+  providers: [
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. "Sign in with...")
+      name: "Login with email",
+
+      credentials: {
+        username: {
+          label: "Username",
+          type: "text",
+          placeholder: "jsmith@gmail.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        // Add logic here to look up the user from the credentials supplied
+        const username = credentials?.username;
+        const password = credentials?.password;
+
+        const user = {
+          name: "Pranav",
+          id: 1,
+          username: "jsmith@gmail.com",
+        };
+
+        console.log(username);
+        console.log(password);
+
+        if (user) {
+          // Any object returned will be saved in `user` property of the JWT
+          return user;
+        } else {
+        // If you return null then an error will be displayed advising the user to check their details.
+        return null;
+
+        // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        }
+      },
+    }),
+    GoogleProvider({
+      clientId: "asd",
+      clientSecret: "asd"
+    }),
+  ],
+
+  secret: process.env.NEXTAUTH_SECRET
+});
+
+export const GET = handler;
+export const POST = handler;
