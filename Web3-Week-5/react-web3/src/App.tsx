11ecfc431c73e1692464bd1c03dcf777618005df
elpranav05 <pranav.awasarmol766@gmail.com>
2026-09-03 19:15:30 +0530

    init web3 week5

---
+++ Web3-Week-5/react-web3/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-black text-white w-screen h-screen flex items-center justify-center">
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
